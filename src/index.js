625e3cb879ad228b4fcde3036f53c5ae5a9fbe75
valma <muceravn@gmail.com>
2022-06-23 17:25:52 +0300

    Adding event listener to reset button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-// Your code here
+// Adding an event listener to the reset button
+const resetButton = document.querySelector('#reset-btn')
+resetButton.addEventListener('click' , (event) =>{
+    fetch("http://localhost:3000/characters")
+    .then (res => res.json())
+    .then (characters =>{
+        const characterName = document.querySelector('#name')
+        const characterID = characters.find(character => character.name === characterName.textContent)
+        fetch(`${"http://localhost:3000/characters"}/${characterID.id}`,{
+            method: 'PATCH',
+            headers: {
+                "Content-Type" : 'application/json'
+            },
+            body:JSON.stringify({
+                votes: 0
+            })
+        })
+        .then(res => res.json())
+        .then(character =>{
+            characterVotes.textContent = character.votes
+            console.log(character.votes)
+        })
+    })
+})
